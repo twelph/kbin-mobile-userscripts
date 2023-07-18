03e6ce99c28f5ab3aaa8a47e500153cf1e48bd71
twelph <twelph@gmail.com>
2023-07-18 06:07:49 -0500

    Create remove-filters.user.js

---
+++ remove-filters.user.js
@@ -0,0 +1,30 @@
+// ==UserScript==
+// @name         Remove Filter Dropdown
+// @namespace    https://github.com/twelph/kbin-mobile-userscripts/
+// @version      1
+// @description  Remove the filter dropdown from kbin.social
+// @author       Twelph
+// @match        https://kbin.social/*
+// @match        https://lab2.kbin.pub/*
+// @match        https://lab3.kbin.pub/*
+// @match        https://fedia.io/*
+// @match        https://karab.in/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    const observer = new MutationObserver((mutationsList, observer) => {
+        for(let mutation of mutationsList) {
+            if (mutation.type === 'childList') {
+                var filterDropdown = document.querySelector('button[aria-label="Filter by type"]').parentNode;
+                if (filterDropdown) {
+                    filterDropdown.parentNode.removeChild(filterDropdown);
+                }
+            }
+        }
+    });
+
+    observer.observe(document.body, { childList: true, subtree: true });
+})();
