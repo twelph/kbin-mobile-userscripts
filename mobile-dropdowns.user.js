8a59f660901cd2a33d5f503ab937fcf24dbf90f9
twelph <twelph@gmail.com>
2023-07-19 04:43:14 -0500

    Update mobile-dropdowns.user.js

--- mobile-dropdowns.user.js
+++ mobile-dropdowns.user.js
@@ -12,20 +12,6 @@
 // @grant        none
 // ==/UserScript==
 
-// ==UserScript==
-// @name         Convert Lists To Dropdown
-// @namespace    https://github.com/twelph/kbin-mobile-userscripts/
-// @version      1
-// @description  Convert lists into a dropdowns on kbin.social
-// @author       Twelph
-// @match        https://kbin.social/*
-// @match        https://lab2.kbin.pub/*
-// @match        https://lab3.kbin.pub/*
-// @match        https://fedia.io/*
-// @match        https://karab.in/*
-// @grant        none
-// ==/UserScript==
-
 (function() {
   'use strict';
 
