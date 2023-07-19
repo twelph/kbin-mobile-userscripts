3b9f84e299502f09454259b691c7e29c2a315129
twelph <twelph@gmail.com>
2023-07-19 07:47:49 -0500

    Update mobile-dropdowns.user.js

--- mobile-dropdowns.user.js
+++ mobile-dropdowns.user.js
@@ -12,26 +12,33 @@
 // @grant        none
 // ==/UserScript==
 
-// Anonymous function to avoid global namespace pollution
+// An immediately invoked function expression (IIFE) to avoid global namespace pollution
 (() => {
-  let optionFound = false;
+  // Flag to keep track of whether option has been found
+  let isOptionFound = false;
 
-  // Utilize MutationObserver to watch for changes in the DOM
+  // Add a style element to initially hide the original menu
+  const styleElement = addStyleElement();
+
+  // Use MutationObserver to watch for changes in the DOM
   const observer = new MutationObserver((mutations) => {
-    if (optionFound) return;
+    if (isOptionFound) return;
 
     mutations.some((mutation) => {
       const { addedNodes } = mutation;
 
-      if (!addedNodes.length) return false;
+      if (addedNodes.length === 0) return false;
 
       const menuElement = document.querySelector('.options__main');
 
       if (!menuElement) return false;
 
-      optionFound = true;
+      isOptionFound = true;
 
       createDropdownMenu(menuElement);
+
+      // Remove the style element to show the dropdown
+      styleElement.parentNode.removeChild(styleElement);
 
       observer.disconnect();
 
@@ -41,80 +48,97 @@
 
   observer.observe(document.body, { childList: true, subtree: true });
 
-  /**
-   * Function to create a dropdown menu
-   *
-   * @param {Element} menuElement - The menu element to replace with a dropdown
-   */
+  function addStyleElement() {
+    const styleElement = document.createElement('style');
+    styleElement.innerHTML = '.options__main { display: none; }';
+    document.head.appendChild(styleElement);
+    return styleElement;
+  }
+
   function createDropdownMenu(menuElement) {
+    const dropdown = createDropdown();
+    const dropdownContainer = createDropdownContainer(menuElement, dropdown);
+
+    // Remove scroll element if it exists
+    const scrollElement = document.querySelector('.scroll');
+    if (scrollElement) scrollElement.remove();
+  }
+
+  function createDropdown() {
     const dropdown = document.createElement('select');
     dropdown.className = 'options__main dropdown';
+
+    styleDropdown(dropdown);
+
+    const options = createOptions();
+    options.forEach((option) => dropdown.append(option));
+
+    dropdown.addEventListener('change', handleChange);
+
+    return dropdown;
+  }
+
+  function styleDropdown(dropdown) {
     dropdown.style.border = 'none';
 
     const bodyStyle = getComputedStyle(document.body);
     dropdown.style.backgroundColor = bodyStyle.backgroundColor;
     dropdown.style.color = bodyStyle.color;
-
-    const dropdownContainer = document.createElement('div');
-    const selectedOption = localStorage.getItem('selectedOption');
-    const menuOptions = Array.from(menuElement.children);
-
-    // Check if any option is active
-    const isActive = menuOptions.some((option) => {
-      const childElement = option.firstElementChild;
-      return childElement.classList.contains('active') || selectedOption === childElement.href;
-    });
-
-    const optionElements = [];
-
-    if (!isActive) {
-      const defaultOption = document.createElement('option');
-      defaultOption.textContent = 'Select';
-      defaultOption.value = '';
-      optionElements.push(defaultOption);
-    }
-
-    menuOptions.forEach((option) => {
-      const optionElement = createOptionElement(option);
-      optionElements.push(optionElement);
-    });
-
-    optionElements.forEach((option) => dropdown.append(option));
-
-    dropdownContainer.append(dropdown);
-    dropdownContainer.style.display = 'inline-block';
-    dropdownContainer.style.width = 'auto';
-    menuElement.parentNode.replaceChild(dropdownContainer, menuElement);
-
-    dropdown.addEventListener('change', handleOptionChange);
-
-    const scrollElement = document.querySelector('.scroll');
-
-    if (scrollElement) scrollElement.remove();
   }
 
-  /**
-   * Function to create an option element
-   *
-   * @param {Element} option - The option to create an element for
-   * @returns {Element} The created option element
-   */
-  function createOptionElement(option) {
-    const optionElement = document.createElement('option');
-    const childElement = option.firstElementChild;
+  function createDropdownContainer(menuElement, dropdown) {
+    const container = document.createElement('div');
+    container.style.display = 'inline-block';
+    container.style.width = 'auto';
+    container.append(dropdown);
+    menuElement.parentNode.replaceChild(container, menuElement);
+    return container;
+  }
+
+  function createOptions() {
     const selectedOption = localStorage.getItem('selectedOption');
-    optionElement.textContent = option.textContent.trim();
+    const menuOptions = Array.from(document.querySelector('.options__main').children);
+    const isActive = checkIfActive(menuOptions, selectedOption);
+    const optionElements = [createDefaultOption(isActive), ...menuOptions.map(createOption)];
+
+    return optionElements;
+  }
+
+  function checkIfActive(menuOptions, selectedOption) {
+    return menuOptions.some((option) => {
+      const childElement = option.firstElementChild;
+
+      return childElement.classList.contains('active') || selectedOption === childElement.href;
+    });
+  }
+
+  function createDefaultOption(isActive) {
+    if (isActive) return;
+
+    const defaultOption = document.createElement('option');
+    defaultOption.textContent = 'Select';
+    defaultOption.value = '';
+    return defaultOption;
+  }
+
+  function createOption(menuOption) {
+    const optionElement = document.createElement('option');
+    const childElement = menuOption.firstElementChild;
+    const selectedOption = localStorage.getItem('selectedOption');
+
+    optionElement.textContent = menuOption.textContent.trim();
     optionElement.value = childElement.href;
     optionElement.selected = childElement.classList.contains('active') || selectedOption === optionElement.value;
+
     return optionElement;
   }
 
-  /**
-   * Event handler for option change
-   *
-   * @param {Event} event - The change event
-   */
-  function handleOptionChange(event) {
+  function handleChange(event) {
+    const selectedOption = event.target.value;
+    localStorage.setItem('selectedOption', selectedOption);
+    window.location.href = selectedOption;
+  }
+})();
     localStorage.setItem('selectedOption', event.target.value);
     window.location.href = event.target.value;
   }
