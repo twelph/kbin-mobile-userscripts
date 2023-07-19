877e5000b171ce2d7a5e4324cc10afc19b6605f2
twelph <twelph@gmail.com>
2023-07-19 04:21:36 -0500

    Update mobile-dropdowns.user.js

--- mobile-dropdowns.user.js
+++ mobile-dropdowns.user.js
@@ -12,72 +12,60 @@
 // @grant        none
 // ==/UserScript==
 
+// ==UserScript==
+// @name         Convert Lists To Dropdown
+// @namespace    https://github.com/twelph/kbin-mobile-userscripts/
+// @version      1
+// @description  Convert lists into a dropdowns on kbin.social
+// @author       Twelph
+// @match        https://kbin.social/*
+// @match        https://lab2.kbin.pub/*
+// @match        https://lab3.kbin.pub/*
+// @match        https://fedia.io/*
+// @match        https://karab.in/*
+// @grant        none
+// ==/UserScript==
+
 (function() {
-    'use strict';
+  'use strict';
 
-    // Create a MutationObserver instance
-    var observer = new MutationObserver(function(mutations) {
-        mutations.forEach(function(mutation) {
+  const observer = new MutationObserver(mutations => {
+    mutations.some(mutation => {
+      if (!mutation.addedNodes.length) return;
+      const optionsMenu = document.querySelector('.options__main');
+      if (!optionsMenu) return;
 
-            // Check if new nodes are added
-            if(mutation.addedNodes.length) {
+      const select = document.createElement('select');
+      select.className = 'options__main dropdown';
+      select.style.border = 'none';
 
-                // Select the options menu
-                var optionsMenu = document.querySelector('.options__main');
+      const selectDiv = document.createElement('div');
+      selectDiv.append(select);
+      selectDiv.style.display = 'inline-block';
+      selectDiv.style.width = 'auto';
 
-                // If optionsMenu exists
-                if(optionsMenu) {
-                    // Create a new select element
-                    var select = document.createElement('select');
-                    select.className = 'options__main dropdown';
+      const selectedOption = localStorage.getItem('selectedOption');
 
-                    // Add the select to a new div
-                    var selectDiv = document.createElement('div');
-                    selectDiv.appendChild(select);
+      Array.from(optionsMenu.children).forEach(option => {
+        const selectOption = document.createElement('option');
+        selectOption.textContent = option.textContent.trim();
+        selectOption.value = option.firstElementChild.href;
+        selectOption.selected = option.firstElementChild.classList.contains('active') || selectedOption === selectOption.value;
+        select.append(selectOption);
+      });
 
-                    // Set the style attributes on the div
-                    selectDiv.style.display = 'inline-block';
-                    selectDiv.style.width = 'auto';
+      optionsMenu.parentNode.replaceChild(selectDiv, optionsMenu);
 
-                    // Retrieve selected option from local storage
-                    var selectedOption = localStorage.getItem('selectedOption');
+      select.addEventListener('change', function() {
+        localStorage.setItem('selectedOption', this.value);
+        window.location.href = this.value;
+      });
 
-                    // Iterate over each option in the menu
-                    for (var i = 0; i < optionsMenu.children.length; i++) {
-                        var option = optionsMenu.children[i];
-
-                        // Create a new option element for the select
-                        var selectOption = document.createElement('option');
-                        selectOption.textContent = option.textContent.trim();
-                        selectOption.value = option.firstElementChild.href;
-
-                        // If the current option was previously selected, select it
-                        if (selectedOption === selectOption.value) {
-                            selectOption.selected = true;
-                        }
-
-                        // Add the option to the select
-                        select.appendChild(selectOption);
-                    }
-
-                    // Replace the options menu with the selectDiv
-                    optionsMenu.parentNode.replaceChild(selectDiv, optionsMenu);
-
-                    // Add event listener to change location on selection
-                    select.addEventListener('change', function() {
-                        // Save selected option to local storage
-                        localStorage.setItem('selectedOption', this.value);
-                        window.location.href = this.value;
-                    });
-
-                    // Disconnect the observer when we're done
-                    observer.disconnect();
-                }
-            }
-        });
+      observer.disconnect();
+      return true;
     });
+  });
 
-    // Start observing the document with the configured parameters
-    observer.observe(document.body, { childList: true, subtree: true });
+  observer.observe(document.body, { childList: true, subtree: true });
 
 })();
