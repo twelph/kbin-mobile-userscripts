e487121431e1cf3fbd0db5e6d4bc3180e0fe3dee
twelph <twelph@gmail.com>
2023-07-30 02:52:53 -0500

    Update mobileDropdowns.user.js

--- mobileDropdowns.user.js
+++ mobileDropdowns.user.js
@@ -1,49 +1,56 @@
-const originalState = {};
+let originalState = {};
 
-function init () {
+function init() {
   const settings = getModSettings('mobileDropdowns');
+
   if (settings.filter) {
-    const mainOptionsList = Array.from(document.querySelectorAll('.options__main'));
-    if (!mainOptionsList.length) return;
-    mainOptionsList.forEach((mainOptions, i) => {
-      originalState[`mainOptions${i}`] = mainOptions.cloneNode(true);
-      const select = createSelect(mainOptions);
-      replaceOptions(mainOptions, select);
-    });
+    const mainOptions = document.querySelector('.options__main');
+    if (!mainOptions) return;
+    originalState = {
+      ...originalState,
+      mainOptions: mainOptions.cloneNode(true),
+    };
+    const select = createSelect(mainOptions);
+    replaceOptions(mainOptions, select);
   }
+
   if (settings.view) {
     const scrollElement = document.querySelector('.scroll');
     if (scrollElement) {
-      originalState.scrollDisplay = scrollElement.style.display;
+      originalState = {
+        ...originalState,
+        scrollDisplay: scrollElement.style.display,
+      };
     }
+
     hideScroll();
   }
 }
 
-function teardown () {
-  const mainOptionsList = Array.from(document.querySelectorAll('.options__main'));
+function teardown() {
+  const mainOptions = document.querySelector('.options__main');
   const scrollElement = document.querySelector('.scroll');
-  mainOptionsList.forEach((mainOptions, i) => {
-    if (mainOptions && originalState[`mainOptions${i}`]) {
-      mainOptions.parentNode.replaceChild(originalState[`mainOptions${i}`], mainOptions);
-    }
-  });
+  if (mainOptions && originalState.mainOptions) {
+    mainOptions.parentNode.replaceChild(originalState.mainOptions, mainOptions);
+  }
+
   if (scrollElement && originalState.scrollDisplay) {
     scrollElement.style.display = originalState.scrollDisplay;
   }
 }
 
-function createSelect (mainOptions) {
+function createSelect(mainOptions) {
+  const parentStyles = getComputedStyle(mainOptions.parentElement);
   const select = document.createElement('select');
   select.className = 'options__main dropdown';
-  select.style.cssText = 'margin-left: 2px; height: fit-content; font-size: 0.9em; padding: 9px; margin-bottom: 2px; width: 30%;';
+  select.style.cssText = `border:1px solid #000;background-color:${parentStyles.backgroundColor};color:${parentStyles.color};`;
   addDefaultOption(select);
   addOptions(mainOptions, select);
   select.onchange = handleSelectChange;
   return select;
 }
 
-function addDefaultOption (select) {
+function addDefaultOption(select) {
   const defaultOption = document.createElement('option');
   defaultOption.textContent = 'Select an Option';
   defaultOption.value = '';
@@ -53,38 +60,37 @@ function addDefaultOption (select) {
   select.appendChild(defaultOption);
 }
 
-function addOptions (mainOptions, select) {
-  const options = Array.from(mainOptions.children);
-  options.forEach(option => {
+function addOptions(mainOptions, select) {
+  Array.from(mainOptions.children).forEach((option) => {
     const newOption = document.createElement('option');
     newOption.textContent = option.textContent.trim();
-    newOption.value = option.firstElementChild.getAttribute('href');
+    newOption.value = option.firstElementChild.href;
     newOption.selected = option.firstElementChild.classList.contains('active');
     select.appendChild(newOption);
   });
 }
 
-function handleSelectChange (event) {
+function handleSelectChange(event) {
   if (event.target.value !== '') {
-    window.location.assign(event.target.value);
+    window.location.href = event.target.value;
   }
 }
 
-function replaceOptions (mainOptions, select) {
+function replaceOptions(mainOptions, select) {
   const divElement = document.createElement('div');
-  divElement.style.display = 'inline-block';
+  divElement.style.cssText = 'display:inline-block;width:auto;';
   divElement.appendChild(select);
   mainOptions.parentNode.replaceChild(divElement, mainOptions);
 }
 
-function hideScroll () {
+function hideScroll() {
   const scrollElement = document.querySelector('.scroll');
   if (scrollElement) {
     scrollElement.style.display = 'none';
   }
 }
 
-function mobileDropdownsInit (toggle) {
+function mobileDropdownsInit(toggle) {
   if (toggle) {
     init();
   } else {
